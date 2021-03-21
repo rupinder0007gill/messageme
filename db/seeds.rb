b3f5aaa411142701aea8b1be70c6e098914dd643
Rupinder Gill <rupinder0007gill@gmail.com>
2021-03-21 16:22:52 +0530

    users_controller_model_seed_hirbgem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Aman", password: "password")
+User.create(username: "Hsg", password: "password")
+User.create(username: "Jashan", password: "password")
+User.create(username: "Gaggu", password: "password")
+User.create(username: "Sandhu", password: "password")
