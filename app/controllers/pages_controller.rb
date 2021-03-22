f6365ff2f3369cd028c67f55a561f9fe66329ae7
Rupinder Gill <rupinder0007gill@gmail.com>
2021-03-22 13:24:58 +0530

    add_message_partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
 	
+	def home
+		@messages = Message.all
+	end
+	
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-	def new
-
+	def home
+		@messages = Message.all
 	end
 end
