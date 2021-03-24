d114bf9c923e0bb3efa81ec454c5e69f09b12f1d
Rupinder Gill <rupinder0007gill@gmail.com>
2021-03-24 17:54:39 +0530

    sessio_create_destroy_flash_messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
 	def home
 		@messages = Message.all
 	end
+
+	def create
+		user = User.find_by( username: params[:session][:username] )
+		if user && user.authenticate(params[:session][:password] )
+			session[:user_id] = user.id
+			flash[:success] = "You have been logged in successfully"
+			redirect_to root_path
+		else 
+			flash.now[:error] = "Invalid credentials"
+			render 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have been logged out successfully"
+		redirect_to login_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
